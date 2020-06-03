719c1aade9d02a0f54b46c54fdf7067cb4968f2a
Michael Vega <micha3lvega@gmail.com>
2020-06-03 12:09:49 -0500

    propety binding

--- src/app/personas/personas.component.ts
+++ src/app/personas/personas.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-personas',
   templateUrl: './personas.component.html',
   styleUrls: ['./personas.component.css'],
 })
-export class PersonasComponent {}
+export class PersonasComponent implements OnInit {
+  disableButton = true;
+  labelButton = 'Disable';
+  classButton = 'btn btn-danger';
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.disableButton = false;
+      this.labelButton = 'Enable';
+      this.classButton = 'btn btn-primary';
+    }, 3000);
+  }
+}
