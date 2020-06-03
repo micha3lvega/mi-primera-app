a8c94b77e07488c754383461a539231463cdb3cd
Michael Vega <micha3lvega@gmail.com>
2020-06-03 11:21:07 -0500

    creacion de componente InlineComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,16 @@ import { AppComponent } from './app.component';
 import { PersonasComponent } from './personas/personas.component';
 import { PersonaComponent } from './persona/persona.component';
 import { SimpleComponent } from './simple/simple.component';
-import { BackTickComponent } from './back-tick/back-tick.component';
+import { BackTickComponent } from './inline/inline.component';
 
 @NgModule({
-  declarations: [AppComponent, PersonasComponent, PersonaComponent, SimpleComponent, BackTickComponent],
+  declarations: [
+    AppComponent,
+    PersonasComponent,
+    PersonaComponent,
+    SimpleComponent,
+    InlineComponent,
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/inline/inline.component.ts
+++ src/app/inline/inline.component.ts
@@ -2,13 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 // para usar en el template mas de una linea se necesita usar el backtick
 @Component({
-  selector: 'app-back-tick',
+  selector: 'app-inline',
   template: `<span>Back</span>
     <br />
     <span>Tick</span>`,
+  styles: [
+    `
+      span {
+        color: red;
+      }
+    `,
+  ],
 })
-export class BackTickComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class InlineComponent {}
