08a99930276c25444dcd822ec9c4a566eb2b1666
Michael Vega <micha3lvega@gmail.com>
2020-06-16 15:06:17 -0500

    create object persona

---
+++ src/app/persona/persona.model.ts
@@ -0,0 +1,8 @@
+export class Persona {
+  public name: string;
+  public lastName: string;
+  constructor(name: string, lastName: string) {
+    this.name = name;
+    this.lastName = lastName;
+  }
+}
